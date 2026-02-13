e5f386bdcf62532eb5384ca9aaed8f42cfd9d9c7
James Whitehead <james.whitehead2@icloud.com>
2026-02-14 09:08:16 +1000

    [Gradle] [Spring] Initialize project

---
+++ src/test/kotlin/com/mable/bank/BankApplicationTest.kt
@@ -0,0 +1,10 @@
+package com.mable.bank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankApplicationTest {
+	@Test
+	fun contextLoads() = Unit
+}
