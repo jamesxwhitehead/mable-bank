f169155154c98dafe35f35696739a322efc7f6ef
James Whitehead <james.whitehead2@icloud.com>
2026-02-17 09:56:41 +1000

    Remove incorrect ObjectMapper bean override from AppConfig, rely on instance provided by Spring Boot autoconfiguration

--- src/main/kotlin/com/mable/bank/config/AppConfig.kt
+++ src/main/kotlin/com/mable/bank/config/AppConfig.kt
@@ -1,15 +1,10 @@
 package com.mable.bank.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
-import tools.jackson.databind.ObjectMapper
 
 @Configuration
 @EnableAsync
 @EnableScheduling
-class AppConfig {
-    @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper()
-}
+class AppConfig
