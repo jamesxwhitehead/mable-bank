2efab993433ad5fc6aa55d1bfa5aa3a8ab25e6ff
James Whitehead <james.whitehead2@icloud.com>
2026-02-17 10:03:32 +1000

    Simplify AccountBalanceDto validation constraints

--- src/main/kotlin/com/mable/bank/dto/AccountBalanceDto.kt
+++ src/main/kotlin/com/mable/bank/dto/AccountBalanceDto.kt
@@ -1,15 +1,11 @@
 package com.mable.bank.dto
 
-import jakarta.validation.constraints.Digits
-import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.PositiveOrZero
-import jakarta.validation.constraints.Size
 import java.math.BigDecimal
 
 data class AccountBalanceDto(
-    @Digits(integer = 16, fraction = 0)
-    @Min(1000000000000000L)
-    @Size(min = 16, max = 16)
+    @Pattern(regexp = "^[1-9]\\d{15}$", message = "Account ID must be a 16-digit number not starting with 0.")
     val accountId: String,
 
     @PositiveOrZero
