12dca3289da8a13f98326ffff97a5babdce79780
James Whitehead <james.whitehead2@icloud.com>
2026-02-26 15:41:29 +1000

    [Gradle] Require net.datafaker:datafaker
    - Introduce dedicated factory class for Faker initialization.
    - Implement Faker bean for application context in AppConfig.

--- src/main/kotlin/com/mable/bank/config/AppConfig.kt
+++ src/main/kotlin/com/mable/bank/config/AppConfig.kt
@@ -1,5 +1,8 @@
 package com.mable.bank.config
 
+import com.mable.bank.config.factory.FakerFactory
+import net.datafaker.Faker
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -7,4 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @Configuration
 @EnableAsync
 @EnableScheduling
-class AppConfig
+class AppConfig {
+    @Bean
+    fun faker(): Faker = FakerFactory.create()
+}
---
+++ src/main/kotlin/com/mable/bank/config/factory/FakerFactory.kt
@@ -0,0 +1,19 @@
+package com.mable.bank.config.factory
+
+import net.datafaker.Faker
+import java.util.Locale
+import java.util.Random
+
+class FakerFactory {
+    companion object {
+        private const val LOCALE_LANGUAGE = "en"
+        private const val LOCALE_COUNTRY = "AU"
+        private const val SEED = 1L
+
+        @JvmStatic
+        fun create(): Faker = Faker(
+            Locale.of(LOCALE_LANGUAGE, LOCALE_COUNTRY),
+            Random(SEED)
+        )
+    }
+}
